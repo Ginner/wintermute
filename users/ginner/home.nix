cc64ef540423fa36977b070d2c7549aadfc48a17
Ginner <26798615+Ginner@users.noreply.github.com>
2025-02-19 08:24:31 +0100

    Added taskfinder to home-manager?

--- users/ginner/home.nix
+++ users/ginner/home.nix
@@ -17,6 +17,7 @@
   home.homeDirectory = "/home/ginner";
 
   home.packages = with pkgs; [
+    inputs.taskfinder.packages.${pkgs.system}.default
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

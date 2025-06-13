74fd6e0509037730f14fd45f86f089eda9655d15
Ginner <26798615+Ginner@users.noreply.github.com>
2025-06-13 05:47:48 +0200

    Started restructuring home-manager

--- homeManagerModules/cliPrograms/kitty.nix
+++ homeManagerModules/cliPrograms/kitty.nix
@@ -1,15 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.myHomeModules.kitty;
+in
 {
-  programs.kitty = {
-    enable = true;
-    # font = {
-    #   name = "Hack Nerd Font Mono";
-    #   size = 11.0;
-    # };
-    shellIntegration.enableZshIntegration = true;
-    # themeFile = "Monokai_Pro";
-    extraConfig = "
-      window_padding_width 10
-      ";
+  options.myHomeModules.kitty = {
+    enable = lib.mkEnableOption "Kitty terminal emulator";
+
+    enableZshIntegration = lib.mkEnableOption "Enable Zsh shell integration for Kitty";
+
+    extraConfig = lib.mkOption {
+      type = lib.types.str;
+      default = "
+        window_padding_width 10
+        ";
+      description = "Extra configuration for Kitty terminal.";
+    };
   };
 
+  config = lib.mkIf cfg.enable {
+    myHomeModules.kitty = {
+      enable = true;
+      enableZshIntegration = cfg.enableZshIntegration;
+      extraConfig = cfg.extraConfig;
+    };
+  };
 }
--- homeManagerModules/cliPrograms/zsh.nix
+++ homeManagerModules/cliPrograms/zsh.nix
@@ -1,39 +1,87 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.myHomeModules.cli.zsh;
+in
 {
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    # enableAutosuggestions = true;
-    enableCompletion = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-    syntaxHighlighting.enable = true;
-    history = {
-      ignoreDups = true;
-      expireDuplicatesFirst = true;
-      ignoreSpace = true;
-      share = true;
-      size = 10000;
-      path = "${config.home.homeDirectory}/.local/share/zsh/history";
-      };
-    shellAliases = {
-      history = "history 1";
-      ls = "eza";
-      ll = "eza -l";
-      lt = "eza -T";
-      la = "eza -lah --group-directories-first";
-      las = "eza -lah --group-directories-first --total-size";
-      cal = "cal -wm";
-      cp = "cp -riv";
-      mv = "mv -iv";
-      rm = "rm -I";
-      mkdir = "mkdir -vp";
-      ":q" = "exit";
+  options.myHomeModules.cli.zsh = {
+    enable = lib.mkEnableOption "Zsh shell";
+
+    enableAutosuggestions = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable zsh autosuggestions";
     };
-    initExtra = ''
-      setopt histverify
-      setopt correct
-      eval "$(direnv hook zsh)"
-    '';
+
+    enableSyntaxHighlighting = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable zsh syntax highlighting";
+    };
+
+    enableCompletion = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable zsh completion";
+    };
+
+    enableAutocd = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable autocd in zsh";
+    };
+
+    dotDir = lib.mkOption {
+      type = lib.types.str;
+      default = ".config/zsh";
+      description = "Directory for zsh configuration files";
+    };
+
+    extraInit = lib.mkOption {
+      type = lib.types.lines;
+      default = ''
+        setopt histverify
+        setopt correct
+        eval "$(direnv hook zsh)"
+      '';
+      description = "Extra initialization commands for zsh";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = cfg.enableAutosuggestions;
+      enableSyntaxHighlighting = cfg.enableSyntaxHighlighting;
+      enableCompletion = cfg.enableCompletion;
+      autocd = cfg.autocd;
+      dotDir = "${config.home.homeDirectory}" + "/${cfg.dotDir}";
+      history = {
+        ignoreDups = lib.mkDefault true;
+        expireDuplicatesFirst = lib.mkDefault true;
+        ignoreSpace = lib.mkDefault true;
+        share = lib.mkDefault true;
+        size = lib.mkDefault 10000;
+        path = "${config.home.homeDirectory}/.local/share/zsh/history";
+      };
+      shellAliases = lib.mkDefault {
+        history = "history 1";
+        ls = "eza";
+        ll = "eza -l";
+        lt = "eza -T";
+        la = "eza -lah --group-directories-first";
+        las = "eza -lah --group-directories-first --total-size";
+        cal = "cal -wm";
+        cp = "cp -riv";
+        mv = "mv -iv";
+        rm = "rm -I";
+        mkdir = "mkdir -vp";
+        ":q" = "exit";
+      };
+      initExtra = cfg.extraInit;
+    };
+    environment.systemPackages = with pkgs; [
+      eza
+    ];
   };
 }

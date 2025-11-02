734aae970f979ddc65423bf98b0a5f70a4ca06e6
Ginner <26798615+Ginner@users.noreply.github.com>
2025-11-02 06:16:46 +0100

    Enable tailscale

--- hosts/BISHOP/configuration.nix
+++ hosts/BISHOP/configuration.nix
@@ -35,6 +35,8 @@
     image = ../../assets/wall.jpeg;
   };
 
+  myModules.services.tailscale.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

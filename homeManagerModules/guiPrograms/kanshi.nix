22b88bec05da378329bc3446e65dc42f6fde18ed
Ginner <26798615+Ginner@users.noreply.github.com>
2025-11-09 20:51:36 +0100

    Cleanup home.nix, fix to kanshi

--- homeManagerModules/guiPrograms/kanshi.nix
+++ homeManagerModules/guiPrograms/kanshi.nix
@@ -21,7 +21,7 @@
           profile.outputs = [
             { criteria = "eDP-1"; position = "0,0"; scale = 1.00; }
             { criteria = "Dell Inc. DELL U2717D T4F1X87A735S"; position = "1920,480"; scale = 1.00; }
-            { criteria = "Dell Inc. DELL U2417H 5K9YD734A3ES"; position = "4480,530"; scale = 1.00; transform = "90"; }
+            { criteria = "Dell Inc. DELL U2417H 5K9YD734A3ES"; position = "4480,530"; scale = 1.00; transform = "270"; }
           ];
         }
 
--- hosts/BISHOP/home.nix
+++ hosts/BISHOP/home.nix
@@ -13,27 +13,11 @@
   # Override stylix wallpaper for BISHOP
   myHomeModules.guiPrograms.stylix.image = ../../assets/wall.jpeg;
 
-  # Additional packages not covered by laptop bundle
+  # Additional packages not covered by the bundles
   home.packages = with pkgs; [
-    neomutt
-    eza
-    poppler
-    zoxide
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   programs.git = {
@@ -45,11 +29,6 @@
   };
 
   # Override SSH configuration with host-specific settings
-  # myHomeModules.cliPrograms.ssh.extraConfig = ''
-  #   Host github.com
-  #     User git
-  #     IdentityFile ~/.ssh/id_ed25519_sk
-  # '';
   myHomeModules.cliPrograms.ssh = {
     enable = true;
     matchBlocks = {
@@ -65,25 +44,6 @@
       };
     };
   };
-  # programs.rofi = {
-  #   enable = true;
-  #   package = pkgs.rofi-wayland;
-  # };
-
-  # Starship configuration is handled by the laptop module
-
-  # xremap configuration moved to system-level modules
-  #
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ginner/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  # Host-specific configuration
+  
   home.stateVersion = "22.11"; # Set based on when this host was installed
 }

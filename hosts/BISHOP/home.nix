c06bbfad672b19dbfa8f85528b447ee535309544
Ginner <26798615+Ginner@users.noreply.github.com>
2025-11-10 09:27:29 +0100

    Adjust screen brightness when docking/un-docking

--- hosts/BISHOP/home.nix
+++ hosts/BISHOP/home.nix
@@ -26,6 +26,7 @@
       profile.outputs = [
         { criteria = "eDP-1"; scale = 1.2; status = "enable"; }
       ];
+      profile.exec = "brightnessctl set 40%";
     }
     { 
       profile.name = "office";
@@ -34,6 +35,7 @@
         { criteria = "Dell Inc. DELL U2717D T4F1X87A735S"; position = "1920,-240"; scale = 1.00; }
         { criteria = "Dell Inc. DELL U2417H 5K9YD734A3ES"; position = "4480,-290"; scale = 1.00; transform = "270"; }
       ];
+      profile.exec = "brightnessctl set 75%";
     }
   ];
 

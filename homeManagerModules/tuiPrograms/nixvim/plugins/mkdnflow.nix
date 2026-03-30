8467fc07923b6915b2ed07886c9fd34f98087517
Ginner <26798615+Ginner@users.noreply.github.com>
2026-03-30 22:29:42 +0200

    mkdnflow todo-list updates

--- homeManagerModules/tuiPrograms/nixvim/plugins/mkdnflow.nix
+++ homeManagerModules/tuiPrograms/nixvim/plugins/mkdnflow.nix
@@ -13,16 +13,27 @@
         bib = false;
         conceal = false;
       };
-      to_do.symbols = [
-        " "
-        "/"
-        "x"
-      ];
+      to_do = {
+        statuses = {
+          not_started = {
+            marker = " ";
+            sort = { section = 2; position = "top"; };
+          };
+          in_progress = {
+            marker = "/";
+            sort = { section = 1; position = "bottom"; };
+          };
+          complete = {
+            marker = [ "X" "x" ];
+            sort = { section = 1; position = "bottom"; };
+          };
+        };
+      };
       mappings = {
         MkdnCreateLink = [ [ "n" ] "<leader>ln" ];
         MkdnEnter = [ [ "i" "n" "v" ] "<cr>" ];
         MkdnToggleToDo = [ ["n" "i"] "<c-space>" ];
-        };
       };
     };
+  };
 }
